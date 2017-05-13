b1b4af98c1caa685916026b582f22cd3649cb55f
buzheng <buzheng@buzhengdeMBP.lan>
2017-05-13 20:33:36 +0800

    setState以及组件生命周期

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ var SecondClass = React.createClass({
     }
 });
 
+
 var WebCompoment = React.createClass({
     render: function () {
         return (
@@ -45,7 +46,7 @@ var WebLink = React.createClass({
 });
 
 /**
- *React State状态
+ * React State状态
  * React 把组件看成是一个状态机（State Machines）。通过与用户的交互，实现不同状态，然后渲染 UI，让用户界面和数据保持一致。
  ＊React 里，只需更新组件的 state，然后根据新的 state 重新渲染用户界面（不要操作 DOM）。
  ＊以下实例中创建了 LikeButton 组件，getInitialState 方法用于定义初始状态，也就是一个对象，
@@ -80,7 +81,77 @@ var LikeButton = React.createClass({
     }
 });
 
+//演示setState用法
+
+var ClickCount = React.createClass({
+    getInitialState:function () {
+        return{
+            clickcount:0
+        }
+    }
+    ,
+    handleClick:function () {
+        this.setState(function (state) {
+            return{clickcount:state.clickcount+1}
+        })
+    },
+    render:function () {
+        return(
+            <h1 onClick={this.handleClick}>clickout is {this.state.clickcount}</h1>
+        )
+    }
+});
+
+
+//第二次书写
+
+var HelloCount = React.createClass({
+    getInitialState:function () {
+       return{
+           clickcount:1
+       }
+    },
+    handleClick:function () {
+        this.setState(function (state) {
+            return{
+                clickcount:this.state.clickcount+1
+            }
+        })
+    },
+    render:function () {
+        return(
+            <h1 onClick={this.handleClick}>点击我{this.state.clickcount}次</h1>
+        )
+    }
+});
+
+//组件的生命周期
+var Timer =React.createClass({
+    getInitialState:function () {
+     return{
+         opacity:1.0
+     }
+    },
+    componentDidMount: function () {
+        this.timer = setInterval(function () {
+            var opacity = this.state.opacity;
+            opacity -= .05;
+            if (opacity < 0.1) {
+                opacity = 1.0;
+            }
+            this.setState({
+                opacity: opacity
+            });
+        }.bind(this), 100);
+    },
+    render:function () {
+        return(
+            <h1 style={{opacity: this.state.opacity}}>HelloBuzheng</h1>
+        )
+    }
+});
+
 ReactDOM.render(
-    <LikeButton />,
+    <Timer />,
     document.getElementById('root')
 );
